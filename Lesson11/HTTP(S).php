843164ceea60f06d4a1d2ab00f11f6a85efd7bbb
DmitryGil <demanelson@mail.ru>
2023-03-08 18:01:18 +0300

    add new theme

--- Lesson11/HTTP(S).php
+++ Lesson11/HTTP(S).php
@@ -19,3 +19,63 @@
 //ПРАКТИКА:
 
 header ('Allow: GET, POST') ?>
+
+
+<span>
+    ТЕКСТ ОТВЕТА
+    [{
+    "key":"Date",
+    "value":"Mon, 06 Mar 2023 20:59:30 GMT",
+    "description":"A general header containing the date and time the message was sent.",
+    "enabled":true,"disableEdit":true,
+    "info":"A general header containing the date and time the message was sent."},
+        {
+        "key":"Content-Type",
+        "value":"text/html;
+        charset=UTF-8",
+        "description":"Indicates the resource's media type.",
+        "enabled":true,
+        "disableEdit":true,
+        "info":"Indicates the resource's media type."
+    },{
+        "key":"Content-Length",
+        "value":"0",
+        "description":"An entity header indicating the size of the entity-body, in bytes.",
+        "enabled":true,"disableEdit":true,"info":"An entity header indicating the size of the entity-body, in bytes."
+    },{
+        "key":"Connection",
+        "value":"keep-alive",
+        "description":"A general header specifying whether the current network connection will stay open once the current transaction finishes.",
+        "enabled":true,
+        "disableEdit":true,
+        "info":"A general header specifying whether the current network connection will stay open once the current transaction finishes."
+    },{
+        "key":"Keep-Alive",
+        "value":"timeout=120",
+        "description":"Custom header",
+        "enabled":true,
+        "disableEdit":true,
+        "info":"Custom header"
+    },{
+        "key":"Server",
+        "value":"Apache",
+        "description":"Contains information about how the server handles requests.",
+        "enabled":true,
+        "disableEdit":true,
+        "info":"Contains information about how the server handles requests."
+    },{
+        "key":"Allow",
+        "value":"GET, POST",
+        "description":"An entity header specifying the supported methods for a resource. Must be sent in response to an HTTP 405 - Method Not Allowed response.",
+        "enabled":true,
+        "disableEdit":true,
+        "info":"An entity header specifying the supported methods for a resource. Must be sent in response to an HTTP 405 - Method Not Allowed response."
+    }]
+</span>
+
+<?php
+http_response_code(301);//Функция для принудительного выставления статуса
+//Заметка: HTTP - небезопасный протокол передачи и все данные передаются в открытом виде
+
+
+//HTTPS - безопасный протокол передачи, использующий SSL - сертификат для удостоверения безопасности
