405b545b22b6e27a93c9bf0e808d1bdd2bda917e
DmitryGil <demanelson@mail.ru>
2023-04-04 23:56:00 +0300

    add some bd practice

--- Lesson20/db_practice.php
+++ Lesson20/db_practice.php
@@ -20,3 +20,46 @@
 //для $pdo = new PDO()
 // $pdo
 //    ->preapre('SELECT * FROM products' ['product' => $_POST['product']])
+
+//Пример самостоятельного SQL-запроса по теме:
+//  SELECT p.*, m.name as manufacture_name, s.* FROM products AS p
+//      left join manufactures as m on p.manufacture_id = m.id
+//      left join product_size ps on p.id = ps.product_id
+//      left join sizes as s on s.id = ps.size_id
+//  WHERE s.size = 'XXL'
+
+
+//-----------------ОРГАНИЗАЦИЯ ПОДЛКЮЧЕНИЯ К БД-----------------//
+
+//учебная классика
+$connection = mysqli_connect();
+
+//...
+
+function getProduct($connection, int $id) {
+    mysqli_query($connection, "SELECT * FROM products WHERE product = $id");
+}
+//такая функция будет каждый раз обращаться к БД
+
+
+//нормальный вариант подключения:
+
+function connect() {
+    //С помощью данных для connect в статической переменной можно организовать lazyload
+    static $connection = null;
+    if ($connection === null) {
+        $connection = mysqli_connect() or die('mysql error');
+    }
+    return $connection;
+}
+
+//...
+
+function getProductName(int $id) {
+    mysqli_query(connect(), "SELECT * FROM products WHERE product = $id");
+}
+
+//...
+getProductName(1);
+
+mysqli_close(connect());
